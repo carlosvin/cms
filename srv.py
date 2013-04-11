1258a48b5f9850f0690ad26c3d263424d7c21fa1
Carlos Martín Sánchez <carlosvin@gmail.com>
2013-04-12 00:16:09 +0200

    / ‘.idea/workspace.xml’ / ‘srv.py’

--- srv.py
+++ srv.py
@@ -2,7 +2,10 @@ __author__ = 'carlos'
 
 class Server:
     def __init__(self):
-        self.__test = ""
+        self.__test = "test"
+
+    def run(self):
+        print self.__test
 
 if __name__ == 'main':
     Server().run()
