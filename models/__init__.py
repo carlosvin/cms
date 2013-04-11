25094de7bd96c2d019964ccdd942a66fcf0948c0
Carlos Martín Sánchez <carlosvin@gmail.com>
2013-04-12 00:32:50 +0200

    / ‘.idea/workspace.xml’ / ‘models/__init__.py’

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,12 @@
+import datetime
+
 __author__ = 'carlos'
 
 
 class Content(object):
     def __init__(self, id, title):
         self.id, self._title = id, title
+        self.created = datetime.datetime.now()
+
+        
+class Category(Content):
