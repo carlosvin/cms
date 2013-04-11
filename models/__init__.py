3e56bb46ea7c858fa64f038e6642294318882975
Carlos Martín Sánchez <carlosvin@gmail.com>
2013-04-12 00:33:13 +0200

    / ‘.idea/workspace.xml’ / ‘models/__init__.py’

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,11 @@ __author__ = 'carlos'
 
 class Content(object):
     def __init__(self, id, title):
+        """
+
+        :param id: content identifier
+        :param title: content title
+        """
         self._id, self._title = id, title
         self._created = datetime.datetime.now()
 
