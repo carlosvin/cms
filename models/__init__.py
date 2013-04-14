5fd95f9fb462bec00bddc02d5b83f012964709a1
Carlos Martín Sánchez <carlosvin@gmail.com>
2013-04-14 14:42:34 +0200

    / ‘models/__init__.py’

--- models/__init__.py
+++ models/__init__.py
@@ -29,7 +29,7 @@ class Content:
         self.id = id
         self.title = title
         self.created = datetime.datetime.now()
-        self.pieces = []
+        self.pieces = {}
 
 
 class Category(Content):
