0e124123a9e163a675f494371ac8ca883a5418c1
Carlos Martín Sánchez <carlosvin@gmail.com>
2013-04-14 14:38:31 +0200

    / ‘models/tests.py’

--- models/tests.py
+++ models/tests.py
@@ -20,6 +20,7 @@ class TestContent(unittest.TestCase):
 
     def testCreate (self):
         content = Content (1, 'Title')
+        content.add_piece(Piece('2323'))
 
 
 if __name__ == "__main__":
