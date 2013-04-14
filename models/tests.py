7c70562dcb07e11f5f5df16f44edf728005bffdd
Carlos Martín Sánchez <carlosvin@gmail.com>
2013-04-14 14:34:42 +0200

    / ‘models/tests.py’

--- models/tests.py
+++ models/tests.py
@@ -4,6 +4,7 @@ Created on 14/04/2013
 @author: carlos
 '''
 import unittest
+from models import Content
 
 
 class TestContent(unittest.TestCase):
@@ -17,8 +18,8 @@ class TestContent(unittest.TestCase):
         pass
 
 
-    def testName(self):
-        pass
+    def testCreate (self):
+        Content(1, 'Title')
 
 
 if __name__ == "__main__":
