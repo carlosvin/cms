e261699b614a73a5d866fc2c8f0612e6cc1651e1
Carlos Martín Sánchez <carlosvin@gmail.com>
2013-04-14 20:05:36 +0200

    / ‘settings/paths.py’

--- settings/paths.py
+++ settings/paths.py
@@ -7,4 +7,4 @@ Created on 14/04/2013
 import os
 
 CONTENT_FOLDER_NAME = 'contents'
-CONTENT_PATH = os.path.join('..').join(CONTENT_FOLDER_NAME)
+CONTENT_PATH = os.path.join(os.getcwd(), '..', CONTENT_FOLDER_NAME,)
