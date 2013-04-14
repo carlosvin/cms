b720367d7ff415c36f3be51f97e2d85c17b88ad1
Carlos Martín Sánchez <carlosvin@gmail.com>
2013-04-14 19:45:36 +0200

    / ‘persistence/__init__.py’

--- persistence/__init__.py
+++ persistence/__init__.py
@@ -1,5 +1,10 @@
+from models import Content
 try:
     from lxml import etree
 except ImportError:
     import xml.etree.ElementTree as etree
     
+class ContentXML:
+    
+    def __init__(self, content):
+        if isinstance(content, Content)
