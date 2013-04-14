425fe4df73bb6eea07784526b2bf34708cbeacbf
Carlos Martín Sánchez <carlosvin@gmail.com>
2013-04-14 14:33:23 +0200

    / ‘models/tests.py’ - ‘tests/’ - ‘tests/__init__.py’

---
+++ models/tests.py
@@ -0,0 +1,26 @@
+'''
+Created on 14/04/2013
+
+@author: carlos
+'''
+import unittest
+
+
+class TestCreation(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
